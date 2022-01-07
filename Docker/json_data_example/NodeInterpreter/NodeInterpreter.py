028cef8a1bb40af75f3aa46a371865169c6d45dc
Henrycke B.S <Oschenberk@gmail.com>
2022-01-07 11:05:45 -0300

    Executando próximo node com base no anterior

--- Docker/json_data_example/NodeInterpreter/NodeInterpreter.py
+++ Docker/json_data_example/NodeInterpreter/NodeInterpreter.py
@@ -1,5 +1,7 @@
 
-class Node():
+
+from pprint import pprint
+class Base_Node():
     def __init__(self, data):
         self._type = data['type']
         self._id = data['id']
@@ -9,7 +11,7 @@ class Node():
     def run(*args, **kwargs):
         pass
 
-class MoveNode(Node):
+class MoveNode(Base_Node):
     def __init__(self, data):
         super().__init__(data)
         axis = ['X ', 'Y ', 'Z ']
@@ -22,11 +24,14 @@ class MoveNode(Node):
         self.move = [{"axis":mv[0], "coordinate":mv[1], "channel":0, "await":True} for mv in self._movment]
         self._output = None
     
-    def run(self, **kwargs):
-        print(f"Movendo com {self._controller} para {self.move}")
-        self._output = True
+    def run(self, _id, output_dict):
+        if output_dict[_id]:
+            print(f"Movendo com {self._controller} para {self.move}")
+            self._output = True
+            output_dict[self._output_id] = True
         #machine_objects[self._controller].M_G0(*self.move, nonsync=None if isinstance(self.move[0], tuple) else True)
-
+    def reset(self):
+        self._output = None
 
     # output
     # """ [
@@ -38,7 +43,47 @@ class MoveNode(Node):
     #         },
     # ] """
 
-nodes = [
+class NodeMaker():
+    def __init__(self, node_sheet, avaliable_node_classes, initial_output_dict={"start":True}) -> None:
+        self.pre_nodes = node_sheet["nodes"]
+        self.connections = node_sheet["connections"]
+        self.output_dict = self.reset_dicit = initial_output_dict
+        self.out2in = {n["from"]:n["to"] for n in self.connections}
+        self.in2out = {n["to"]:n["from"] for n in self.connections}
+        self.input_connections = {cn["to"] for cn in self.connections}
+        self.avaliable_nodes = avaliable_node_classes
+        self.node_sequence = []
+        self.node_sequence = self.make_nodes()
+    def reset(self):
+        print('\n', " -- Resetando Nodes -- ", '\n')
+        self.output_dict = self.reset_dicit.copy()
+        for node in self.nodes.values():
+            node.reset()
+    def make_nodes(self):
+        self.node_by_input_id = {}
+        self.node_sequence = []
+        self.nodes = {}
+
+        for node in self.pre_nodes:
+            if node["type"] in self.avaliable_nodes:
+                self.nodes[node["id"]] = self.avaliable_nodes[node["type"]](node)
+                self.node_by_input_id[self.nodes[node["id"]]._input_id] = self.nodes[node["id"]]
+                if self.nodes[node["id"]]._input_id not in self.input_connections:
+                    self.node_sequence.append(node["id"])
+        
+        self.node_sequence = self.node_sequencer(self.node_sequence, self.node_sequence[0])
+        return self.node_sequence
+    def node_sequencer(self, lista, _ids, input=False):
+        try:
+            last_id = self.node_by_input_id[ self.out2in[self.nodes[_ids]._output_id]]._id
+            lista.append(last_id)
+            lista = self.node_sequencer(lista, last_id)
+        except KeyError:
+            pass
+        return lista
+
+template = {
+    "nodes": [
         {
             "type": "MoveNode",
             "id": "node_16414770159090",
@@ -80,7 +125,7 @@ nodes = [
                 [
                     "Saida",
                     {
-                        "id": "AA",
+                        "id": "ni_16414770159093",
                         "value": None
                     }
                 ],
@@ -127,7 +172,7 @@ nodes = [
                 [
                     "Entrada",
                     {
-                        "id": "BB",
+                        "id": "ni_16414770159105",
                         "value": 0
                     }
                 ],
@@ -141,7 +186,7 @@ nodes = [
                 [
                     "Saida",
                     {
-                        "id": "CC",
+                        "id": "ni_16414770159107",
                         "value": None
                     }
                 ],
@@ -188,7 +233,7 @@ nodes = [
                 [
                     "Entrada",
                     {
-                        "id": "DD",
+                        "id": "ni_164147704394713",
                         "value": 0
                     }
                 ],
@@ -222,54 +267,38 @@ nodes = [
             "twoColumn": True,
             "customClasses": ""
         }
-    ]
-
-connections = [
+    ],
+    "connections": [
         {
             "id": "16414770159109",
-            "from": "AA",
-            "to": "BB"
+            "from": "ni_16414770159093",
+            "to": "ni_16414770159105"
         },
         {
             "id": "164147704684718",
-            "from": "CC",
-            "to": "DD"
+            "from": "ni_16414770159107",
+            "to": "ni_164147704394713"
         }
     ]
+}
 
-input_connections = {cn["to"]:None for cn in connections}
-output_connections = {cn["from"]:None for cn in connections}
-links = {cd["from"]:cd["to"] for cd in connections}
-print(links)
-# print(input_connections, output_connections)
-new_nodes = {}
-node_by_input_id = {}
-node_by_output_id = {}
-node_sequence = []
+from itertools import cycle
+node_sheet = NodeMaker(template, {"MoveNode": MoveNode})
+licycle = cycle(node_sheet.node_sequence)
 
-for node in nodes:
-    n = MoveNode(node)
-    new_nodes[n._id] = n
-    node_by_input_id[n._input_id] = new_nodes[n._id]
-    node_by_output_id[n._output_id] = new_nodes[n._id]
-    if n._input_id not in input_connections:
-        node_sequence.append(n._id)
+for i in range(3):
+    node_sheet.reset()
 
+    for _ in range(len(node_sheet.node_sequence)):
+        thisnode = next(licycle)                            #? Acessa o próximo elemento do ciclo (id do nó)
+        node = node_sheet.nodes[thisnode]                   #? Acessa o nó pelo id
+        if thisnode == node_sheet.node_sequence[0]:         #? Se for o primeiro nó, então o input é predefinido
+            _input = "start"
+        else:                                               #? Senão, o input é o output do nó anterior
+            _input = node_sheet.in2out[node._input_id]
 
-def acess(lista, _ids, input=False):
-    try:
-        last_id = node_by_input_id[links[new_nodes[_ids]._output_id]]._id
-        lista.append(last_id)
-        lista = acess(lista, last_id)
-    except KeyError:
-        pass
-    return lista
+        node.run(_input, node_sheet.output_dict)            #? Executa o nó             
+        print("Saida:", node_sheet.output_dict)
 
-node_sequence = acess(node_sequence, node_sequence[0])
-print(node_sequence)
-
-for n in node_sequence:
-    print(new_nodes[n]._input_id, new_nodes[n]._output_id)
-
-#? Sequencia de nodes esá aparetemente correta, ( aplicar nome em algus nodes e verificar se está correto)
-#! Definir como a classe do nó executa a função, e relação de argumentos/ outputs de cada nó.
+        
+#! Cada classe de nó tem um tipo de input e um ou mais formatos aceitos.
