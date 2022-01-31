5be998e11b72f8c1e1aacf9b0303ab3edc3c5e6d
Henrycke B.S <Oschenberk@gmail.com>
2022-01-31 08:59:47 -0300

    [Fix]: Dockerfile, python enviroment variable
    - Remove older docker files
    - add "PYTHON_RUN" to enviroment variables at backend container in dockercompsoe file.

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,14 +1,13 @@
-from re import S
 from src.manager.mongo_manager import connectToMongo, getDb
 from src.manager.socketio_manager import connect_to_socketio, get_io, get_app
 from os import environ
 from time import sleep
-if not environ.get("PYTHON_RUN", True):
-    while True:
-        print("a")
-        sleep(3600)
-        
 
+if environ.get("PYTHON_RUN", "true").lower() == "false":
+    print("Python is not running...")
+    while True:
+        print("Waiting for 20 minutes...")
+        sleep(20*60)
 
 
 connectToMongo()
