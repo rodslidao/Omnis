d5212a0b0c25906c31a49fee3842fd3f2c469612
Henrycke B.S <Oschenberk@gmail.com>
2022-02-01 12:28:53 -0300

    Fix docker dependencies

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from api import dbo
-# exit()
+from starlette.middleware.cors import CORSMiddleware
 from src.logs.log import logSetup
 logger = logSetup("Api")
 try:
@@ -26,26 +26,13 @@ try:
         type_defs, query, mutation, subscription, snake_case_fallback_resolvers
     )
 
-    # app.mount("/graphql", GraphQL(schema, debug=True))
-    app = GraphQL(schema, debug=True)
-    # @app.route("/graphql", methods=["GET"])
-    # app.mount("/graphql", graphql_sync, schema=schema, graphiql=True)
-    # def graphql_playground():
-    #     logger.debug("GraphQL Playground requested")
-    #     return PLAYGROUND_HTML, 200
+    app = CORSMiddleware(GraphQL(schema, debug=True), allow_origins=['*'], allow_methods=['*'], allow_headers=['*'])
 
-
-    # @app.route("/graphql", methods=["POST", "GET"])
-    # def graphql_server():
-    #     data = request.get_json()
-    #     success, result = graphql_sync(schema, data, context_value=request, debug=app.debug)
-    #     status_code = 200 if success else 400
-    #     return jsonify(result), status_code
 
     port = environ["SERVER_PORT"] if environ.get("SERVER_PORT") else 5000
     print("Starting server on port:", port)
     if __name__ == "__main__":
-        uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+        uvicorn.run("main:app", host="0.0.0.0", port=int(port), log_level="info")
     # io.run(app, host="0.0.0.0", port=port)
 
 except KeyboardInterrupt:
