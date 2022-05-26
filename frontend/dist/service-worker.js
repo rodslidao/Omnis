8831dd5b1eb37d508f07776e4c97de53f2af4e75
Omnis <Omnis-Actions@users.noreply.github.com>
2022-05-26 18:11:47 +0000

    Automated report

--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcf249914f452d96214281b76842dfed.js"
+  "/precache-manifest.266866c7124175b9899cdf67c9eb4293.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "omnis"});
