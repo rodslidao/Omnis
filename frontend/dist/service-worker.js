26c7365bfe97a0604f834bd1c2b725efed734d79
Omnis-Builder <Omnis-Actions@users.noreply.github.com>
2022-07-27 11:23:52 +0000

    [build]

--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cba7b24fd454df77c2f88122c0ce764.js"
+  "/precache-manifest.f3413589374be0f7794147bd8e635588.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "omnis"});
