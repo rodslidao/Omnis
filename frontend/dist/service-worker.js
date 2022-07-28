c0e44744257fd1fa00df2c4026bbe9dc79be497e
action-publisher-dist <Omnis-Actions@users.noreply.github.com>
2022-07-28 13:49:16 +0000

    [build]

--- frontend/dist/service-worker.js
+++ frontend/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbcd124e1183bb1628aada9e48438b09.js"
+  "/precache-manifest.6bfef5083d52eb9d7ea1d09f53002ef7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "omnis"});
