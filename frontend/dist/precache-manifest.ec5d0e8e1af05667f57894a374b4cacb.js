867195b714ba9893e2708b4558ddea02129e1d71
action-publisher-dist <Omnis-Actions@users.noreply.github.com>
2022-09-09 22:12:07 +0000

    [build]

--- frontend/dist/precache-manifest.ec5d0e8e1af05667f57894a374b4cacb.js
+++ frontend/dist/precache-manifest.ec5d0e8e1af05667f57894a374b4cacb.js
@@ -4,8 +4,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/css/chunk-vendors.7552040f.css"
   },
   {
-    "revision": "c51bb2723b4c777b0ddc",
-    "url": "/css/index.8c1d533c.css"
+    "revision": "02b5b9b0ef33b2f20ef1",
+    "url": "/css/index.9ceaf3a0.css"
   },
   {
     "revision": "c5c1873073eaadb07d99d4eb037a4ee5",
@@ -160,7 +160,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/users/user.gif"
   },
   {
-    "revision": "e9e59d253675be881516b155c795eb57",
+    "revision": "eb2343005279022da3faeb78c78fe28e",
     "url": "/index.html"
   },
   {
@@ -168,8 +168,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js/chunk-vendors.a13d9c55.js"
   },
   {
-    "revision": "c51bb2723b4c777b0ddc",
-    "url": "/js/index.b6ea6f7c.js"
+    "revision": "02b5b9b0ef33b2f20ef1",
+    "url": "/js/index.09d98c5e.js"
   },
   {
     "revision": "7cfe264e7f1084d3fe8ef2cec97ff0c6",
